62910e66af3e48d5733f96fb7d7e836ebcb7aad6
Rme <devrme01@gmail.com>
2026-08-02 16:13:38 -0300

    funçao busca jogos por PUUID

--- src/chalenger.py
+++ src/chalenger.py
@@ -7,6 +7,7 @@ import requests
 from dotenv import load_dotenv
 from requests.exceptions import RequestException
 import pandas as pd
+import time
 
 HTTP_OK = 200
 
@@ -36,15 +37,46 @@ def validate_connection_get_response(my_api_key: str) -> dict | None:
     return None
 
 
-def transfer_data_to_csv(data_ranking: dict) -> None:
+def transfer_data_to_xlsx(data_ranking: dict) -> None:
     if data_ranking:
         df = pd.DataFrame(data_ranking["entries"])
         df.to_excel("ranking_tft.xlsx", index=False)
         print("Generate document: Sucess!")
 
 
+def generate_puuid_list_from_xlsx(xlsx_path: str) -> list[str]:
+    # le o arquivo xlsx e gera uma lista de PUUID
+    df = pd.read_excel(xlsx_path)
+    return df["puuid"].tolist()
+
+
+def get_matches_by_player(my_api_key: str, my_puuid: str) -> list:
+
+    # gera lista de partidas por PUUID
+    url_matches = f"https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/{my_puuid}/ids?count=10"
+    headers = {"X-Riot-Token": my_api_key}
+    response_matches = requests.get(url_matches, headers=headers, timeout=10)
+    if response_matches.status_code == HTTP_OK:
+        return response_matches.json()
+    return []
+
+
 def main() -> None:
 
     my_api_key = key_validation()
     data_ranking = validate_connection_get_response(my_api_key)
-    transfer_data_to_csv(data_ranking)
+    # gera xlsx com os dados do chalenger
+    transfer_data_to_xlsx(data_ranking)
+
+    xlsx_path = "/home/renan/codigo/TFT/src/ranking_tft.xlsx"
+
+    # pegar partidas por player
+    puuids = generate_puuid_list_from_xlsx(xlsx_path)
+    for p_id in puuids:
+        match_id = get_matches_by_player(my_api_key, p_id)
+        print(f"{match_id}")
+        time.sleep(1.2)
+
+
+if __name__ == "__main__":
+    main()
--- src/test_chalenger.py
+++ src/test_chalenger.py
@@ -1,12 +1,10 @@
 # Copyright (c) 2024 Renan Evaristo
 # ruff: noqa: S101
 
-from unittest.mock import patch
-
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
-
-from chalenger import key_validation
+from unittest.mock import patch, MagicMock
+from src.chalenger import key_validation, transfer_data_to_csv, validate_connection_get_response
 
 
 def test_key_validation_success(monkeypatch: MonkeyPatch) -> None:
@@ -21,6 +19,3 @@ def test_key_validation_failure() -> None:
     with patch("os.getenv", return_value=None), pytest.raises(SystemExit) as e:
         key_validation()
     assert e.value.code == 1
-
-
-def test_validate_connection_get_response() -> None: ...
